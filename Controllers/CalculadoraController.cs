8cb2c58dfaef46b3f6bccd2f1200955c04133173
Augusto Maia <augustofmaia@hotmail.com>
2022-11-30 11:24:22 -0300

    first commit

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,39 @@
+using CalculadoraSimplesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraSimplesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculadoraController : ControllerBase
+    {
+        
+        [HttpGet("Somar/{valor1}/{valor2}")]
+        public ActionResult<Calculadora> Somar(double valor1, double valor2) {
+            var calc = new Calculadora(valor1, valor2);
+            calc.somar();           
+            return Ok(calc);
+        }
+
+        [HttpGet("Subtrair/{valor1}/{valor2}")]
+        public ActionResult<Calculadora> Subtrair(double valor1, double valor2) {
+            var calc = new Calculadora(valor1, valor2);
+            calc.subtrair();           
+            return Ok(calc);
+        }
+
+        [HttpGet("Multiplicar/{valor1}/{valor2}")]
+        public ActionResult<Calculadora> Multiplicar(double valor1, double valor2) {
+            var calc = new Calculadora(valor1, valor2);
+            calc.multiplicar();           
+            return Ok(calc);
+        }
+
+        [HttpGet("Dividir/{valor1}/{valor2}")]
+        public ActionResult<Calculadora> Dividir(double valor1, double valor2) {
+            var calc = new Calculadora(valor1, valor2);
+            calc.dividir();           
+            return Ok(calc);
+        }
+    }
+}
